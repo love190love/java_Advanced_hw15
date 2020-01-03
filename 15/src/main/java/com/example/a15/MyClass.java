3c2bda0c6eefe3a300cbff42f9a38a65430e688a
love190love <love190love@yahoo.com.tw>
2020-01-03 15:07:55 +0800

    Initial commit

---
+++ 15/src/main/java/com/example/a15/MyClass.java
@@ -0,0 +1,19 @@
+package com.example.a15;
+
+
+import java.util.Scanner;
+
+public class MyClass{
+    public static void main(String[] args) {
+        String str = "Taiwan, Touch, Your, Heart";
+        String strChange = "";
+
+        int strlength = str.length();
+        for (int i = 0; i < strlength; i++) {
+            if (str.charAt(i)!=(char)44) {
+                strChange += str.charAt(i);
+            }
+        }
+        System.out.println(strChange);
+    }
+}
